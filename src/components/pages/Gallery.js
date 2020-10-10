db035f566bf56dfb132d5a85699ad69b8d906357
kadjiah <kadijahwilson@pursuit.org>
2020-10-09 20:01:10 -0400

    added pictures for masonary layout

--- src/components/pages/Gallery.js
+++ src/components/pages/Gallery.js
@@ -2,14 +2,28 @@ import React from 'react';
 import '../../App.css';
 import { Button } from '../Button';
 import Footer from '../Footer';
+import cake1 from '../images/cake1.JPG'
+import cake2 from '../images/cake2.JPG'
+import cake3 from '../images/cake3.png'
+import cake4 from '../images/cake4.png'
+import cake5 from '../images/cake5.png'
+import '../css/Gallery.css';
 
-const Gallery =() =>{
+const Gallery = (props) =>{
   return (
-    <>
+    <div  className="gallery">
     <h1 className='SkillsHero'>Wonderfull Cakes</h1>
-    
+
+    <div className="container">
+    <div><img src={cake1} className='BakeryImg'/></div>
+    <div><img src={cake2} className='BakeryImg'/></div>
+    <div><img src={cake3} className='BakeryImg'/></div>
+    <div><img src={cake4} className='BakeryImg'/></div>
+    <div><img src={cake5} className='BakeryImg'/></div>
+    </div>
+
       <Footer/>
-    </>
+    </div>
   );
 }
 
