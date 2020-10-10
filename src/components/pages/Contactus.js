3953c7609b9af8115728b51d5d7a0af7b227b033
kadjiah <kadijahwilson@pursuit.org>
2020-10-09 21:39:09 -0400

    added form with name and a select box

--- src/components/pages/Contactus.js
+++ src/components/pages/Contactus.js
@@ -7,20 +7,33 @@ import Footer from '../Footer';
 
  const SignUp = () =>{
   return(
-    <>
+    <div>
     
     <h1 className='sign-up'>Let's Talk</h1>
     <div className='contact'> 
-      <div className='connect'>
-        
-      </div>
 
-      <div className='phone'>
-       
-      </div>      
+     <form>
+     <label for="fname">First Name</label>
+    <input type="text" id="fname" name="firstname" placeholder="first name.."/>
+    
+    <label for="lname">Last Name</label>
+    <input type="text" id="lname" name="lastname" placeholder="last name.."/>
+
+    <label for="event">Event</label>
+    <select id="event" name="event">
+      <option value="wedding">Wedding</option>
+      <option value="birthday">Birthday</option>
+      <option value="just because">Just Because</option>
+      <option value="babyshower">Babyshower</option>
+      <option value="anniversary">Anniversary</option>
+      <option value="other">Other</option>
+    </select>
+
+     </form>   
+
     </div>
     <Footer />
-      </>
+      </div>
   ) 
 }
 
